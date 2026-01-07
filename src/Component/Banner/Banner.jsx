f48367e9d08ad2a088599b85d80c89dc623ea00d
Afroza-Nipa13 <afrozaakter228@gmail.com>
2026-01-07 08:06:44 +0600

    Updated design in banner section

--- src/Component/Banner/Banner.jsx
+++ src/Component/Banner/Banner.jsx
@@ -1,5 +1,6 @@
 import { easeOut, motion } from "framer-motion";
 import React from "react";
+import { FaDownload } from "react-icons/fa";
 import { ReactTyped } from "react-typed";
 export default function Banner() {
   const cvUrl = "/Afroza_Akter_CV.pdf";
@@ -41,7 +42,7 @@ export default function Banner() {
             I love building interactive, user-friendly, and modern web applications.
           </p>
 
-          <div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
+          {/* <div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
             <button
               onClick={handleDownloadCV}
               className="bg-gradient-to-r from-[#B79891] via-white to-[#94716B] text-gray-900 cursor-pointer px-8 py-3 text-lg font-semibold rounded"
@@ -54,7 +55,17 @@ export default function Banner() {
             >
               View Projects
             </a>
-          </div>
+          </div> */}
+
+          <div className="card-wrapper h-[60px] w-[160px]">
+      <button
+        onClick={handleDownloadCV}
+        className="card-content text-gray-300 flex justify-center items-center font-bold"
+      >
+        <FaDownload className="mr-2 text-sm" />
+        Resume
+      </button>
+    </div>
         </div>
 
         {/* Right Side - Image */}
@@ -65,7 +76,23 @@ export default function Banner() {
            
             className="max-w-sm w-164 rounded-full border-l-4 border-b-4 border-[#94716B] shadow-2xl" />
         </div>
+        
       </div>
+      <motion.svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" className='hidden lg:block'  >
+  <defs>
+    {/* Gradient definition */}
+    <linearGradient id="waveGradient" x1="20%" y1="0%" x2="100%" y2="100%">
+      <stop offset="0%" style={{ stopColor: "#6c757d", stopOpacity: 1 }} />
+      <stop offset="47%" style={{ stopColor: "#0d1321", stopOpacity: 1 }} />
+      <stop offset="100%" style={{ stopColor: "#000814", stopOpacity: 1 }} />
+    </linearGradient>
+  </defs>
+
+  <path
+    fill="url(#waveGradient)"
+    d="M0,256L30,266.7C60,277,120,299,180,282.7C240,267,300,213,360,197.3C420,181,480,203,540,229.3C600,256,660,288,720,293.3C780,299,840,277,900,229.3C960,181,1020,107,1080,117.3C1140,128,1200,224,1260,234.7C1320,245,1380,171,1410,133.3L1440,96L1440,320L1410,320C1380,320,1320,320,1260,320C1200,320,1140,320,1080,320C1020,320,960,320,900,320C840,320,780,320,720,320C660,320,600,320,540,320C480,320,420,320,360,320C300,320,240,320,180,320C120,320,60,320,30,320L0,320Z"
+  />
+</motion.svg>
     </section>
   );
 }
